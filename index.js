1c5802036697b8bed30de1740497c21ed1598bc6
Harsha Sandakelum <mmharshasandakelum@gmail.com>
2023-11-11 18:06:52 +0530

    set heade, footer and all the main sections

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import express from "express";
 const port = 3000;
 const app = express();
 
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
 app.listen(port, () => {
     console.log(`server runining in port: ${port}`);
 });

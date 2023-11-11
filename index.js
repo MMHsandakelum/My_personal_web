0ff1abb47fd5ccdce877672088100f97f9710373
Harsha Sandakelum <mmharshasandakelum@gmail.com>
2023-11-11 19:35:12 +0530

    navbar complete

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import express from "express";
 const port = 3000;
 const app = express();
 
+
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
-
+app.use(express.static('public'));
 app.listen(port, () => {
     console.log(`server runining in port: ${port}`);
 });
